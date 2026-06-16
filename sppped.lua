419be110948ada627e1d11572b6d9832a3db7b08
kuzenontop <ignatiusjourn@gmail.com>
2026-06-16 16:32:57 +0800

    Create sppped.lua

---
+++ sppped.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kuzenontop/SpeedHub/refs/heads/main/SpeedHub.lua"))()
+task.wait(3)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
